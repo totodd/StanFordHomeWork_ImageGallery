2046f5effaa502e1963c8b588f54fd037cd39f94
Tao Chen <tao@quizlingapp.com>
2019-01-06 00:26:08 +1100

    Initial Commit

---
+++ StanFordHomeWork_ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StanFordHomeWork_ImageGallery
+//
+//  Created by TOTO on 6/1/19.
+//  Copyright © 2019 TOTO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
